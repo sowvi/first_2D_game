a6be4609c08738e607885d9c472554964f345021
sowvi <ragabm488@gmail.com>
2022-12-28 02:39:19 +0200

    commit

--- include/RenderWindow.hpp
+++ include/RenderWindow.hpp
@@ -4,10 +4,13 @@
 
 class RenderWindow
 {
-public:
-	RenderWindow(const char* p_title,int p_w,int p_h);
+
 	private:
 SDL_Window* window;
 SDL_Renderer* renderer;
+public:
+	RenderWindow(const char* p_title,int p_w,int p_h);
+
+
 	
 };
